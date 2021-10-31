e86efc422cf19e6880c61b578db128dfeee68240
JRF344 <Joshrf344@gmail.com>
2021-10-31 15:45:41 -0600

    Validation Refactoring
    - Added verification code validation
    - Refactored 'recuperar-password.js' into 'servicio-validar-login.js' and made it work with any login form.
    - Renamed variables to make them more generic and recyclable
    - Made error messages more generic and recyclable
    - General code formatting

--- js/servicio-validar-login.js
+++ js/servicio-validar-login.js
@@ -1,37 +1,49 @@
 'use strict';
 
-const botonRecuperar = document.querySelector('#btn-recuperar');
-const inputCorreo = document.querySelector('#txt-correo');
+const botonForm = document.querySelector('#btn-account-form');
 
-const validarEmail = (email) =>{
+const inputCorreo = document.querySelector('#txt-correo');
+const inputPassword = document.querySelector('#txt-password');
+
+
+function validarEmail(email) {
     let resultado = false;
     const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     resultado = re.test(email);
     return resultado;
 }
 
-
 const validar = () => {
     let error = false;
 
     // Condición que valida el correo
     if (validarEmail(inputCorreo.value)) {
-        document.querySelector('.input-email').classList.remove('input-error');     
+        document.querySelector('.input-email').classList.remove('input-error');
     } else {
         error = true;
         document.querySelector('.input-email').classList.add('input-error');
+    }
+
+    //Condicion para validar la contraseña (SOLO VALIDA SI EXISTE)
+    if (inputPassword !== null) {
+        if (inputPassword.value == '') {
+            error = true;
+            document.querySelector(".input-password").classList.add("input-error");
+        } else {
+            document.querySelector(".input-password").classList.remove("input-error");
+        }
     }
 
 
     if (error == true) {
         Swal.fire({
             'icon': 'warning',
-            'title': 'El correo introducido es invalido',
-            'text': 'Por favor revise los campos resaltados',
+            'title': 'La informacion intruducida es invalida',
+            'text': 'Por favor revise los campos resaltados.',
             'confirmButtonText': 'Entendido'
         });
 
-    } else {
+    } /* else {
         Swal.fire({
             'icon': 'success',
             'title': 'Se ha enviado un correo de recuperacion a la siguiente direccion:',
@@ -41,10 +53,9 @@ const validar = () => {
             // Redireccionamos al dashboard luego de darle click al botón del sweet alert
             // window.location.href = 'dashboard.html';
         });
-    }
+    } */
 
 };
 
 
-
-botonRecuperar.addEventListener('click', validar);
+botonForm.addEventListener('click', validar);
