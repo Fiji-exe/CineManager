6ffb954a3b9180087b4566efe52cc15c71a8f0a6
adriand00 <amurillog@ucenfotec.ac.cr>
2021-11-07 10:38:18 -0600

    Buscar Listar javascript
    adding json hard coded data

--- js/buscar-listar.js
+++ js/buscar-listar.js
@@ -1,73 +1,145 @@
 'use strict';
+let listaCartelera = [{
+        'id': 'Codigo Cartelera',
+        'pelicula': 'Pelicula',
+        'cine': 'Cine',
+        'sala': 'Tipo de Sala',
+        'inicia': 'Hora Inicio',
+        'finaliza': 'Hora Fin'
+    },
+    {
+        'id': 'vres897',
+        'pelicula': 'Wall-E',
+        'cine': 'Magaly',
+        'sala': '3D',
+        'inicia': '10:00',
+        'finaliza': '11:30'
+    },
+    {
+        'id': 'c951moe',
+        'pelicula': 'Heathers',
+        'cine': 'Nova Cinemas',
+        'sala': 'IMAX',
+        'inicia': '13:30',
+        'finaliza': '15:30'
+    }
+]
+let listaPeliculas = [{
+        'titulo': 'Titulo de Pelicula',
+        'imagen': 'Poster',
+        'duracion': 'Duracion (min.)',
+        'categoria': 'Categoria Pelicula',
+        'sinopsis': 'Sinopsis',
+        'idioma': 'Idioma',
+        'subtitulo': 'Subtitulos',
+        'perfil': 'Perfil'
+    },
+    {
+        'titulo': 'Wall-E',
+        'imagen': '',
+        'duracion': '90',
+        'categoria': 'animacion',
+        'sinopsis': 'W3Schools is optimized for learning and training. Examples might be simplified to improve reading and learning. Tutorials, references, and examples are constantly reviewed to avoid errors, but we cannot warrant full correctness of all content. While using W3Schools, you agree to have read and accepted our terms of use, cookie and privacy policy.',
+        'idioma': 'ingles'
+    },
+    {
+        'titulo': 'Heathers',
+        'imagen': '',
+        'duracion': '120',
+        'categoria': 'musical',
+        'sinopsis': 'W3Schools is optimized for learning and training. Examples might be simplified to improve reading and learning. Tutorials, references, and examples are constantly reviewed to avoid errors, but we cannot warrant full correctness of all content. While using W3Schools, you agree to have read and accepted our terms of use, cookie and privacy policy.',
+        'idioma': 'portugues'
+    },
+
+]
+let listaCines = [{
+        'cadena': 'Nombre de Cadena',
+        'codigo': 'Codigo de Cadena',
+        'ubicacion': 'Ubicacion',
+        'margen': 'Margen de Ganancia',
+        'perfil': 'Perfil de Cadena'
+    },
+    {
+        'Cadena': 'La Momia',
+        'imagen': '',
+        'duracion': '180',
+        'categoria': 'accion',
+        'sinopsis': 'W3Schools is optimized for learning and training. Examples might be simplified to improve reading and learning. Tutorials, references, and examples are constantly reviewed to avoid errors, but we cannot warrant full correctness of all content. While using W3Schools, you agree to have read and accepted our terms of use, cookie and privacy policy.',
+        'idioma': 'español'
+    }
+]
 let listaUsuarios = [{
     'nombre': 'Pablo Monestel',
     'correo': 'pablo@gmail.com',
     'contrasenna': '123abc',
     'nacimiento': '1987-10-12',
     'edad': 34,
-    'foto': '',
+    'avatar': '',
     'rol': 1,
-    'estado': 1,
-    'telefonos': [
-        { 'numero': '8888-9766' },
-        { 'numero': '9999-9766' }
-    ]
+    'estado': 1
 }, {
     'nombre': 'Alejandro Coto',
     'correo': 'alejandro@gmail.com',
     'contrasenna': '123abc',
     'nacimiento': '1966-09-01',
     'edad': 55,
-    'foto': '',
+    'avatar': '',
     'rol': 2,
-    'estado': 1,
-    'telefonos': [
-        { 'numero': '8128-7336' }
-    ]
+    'estado': 1
 }, {
     'nombre': 'Belén Acuña',
     'correo': 'belen@gmail.com',
     'contrasenna': '123abc',
     'nacimiento': '2000-03-19',
     'edad': 21,
-    'foto': '',
+    'avatar': '',
     'rol': 3,
-    'estado': 1,
-    'telefonos': [
-        { 'numero': '2234-8865' },
-        { 'numero': '2222-1146' },
-        { 'numero': '7098-3371' }
-    ]
+    'estado': 1
 }];
 
-const obtenerUsuarios = () => listaUsuarios;
+;
 
-const cuerpoTabla = document.querySelector('#tabla-uno');
-let lista = obtenerUsuarios();
+const tabla = document.querySelector('#tabla-uno');
+const js_checar_inputs = () => {
+    let filtro = document.querySelector('#tabla-uno');
+    let keyword = document.querySelector('#tabla-uno');
+}
 
-const mostrarTabla = () => {
-    lista.forEach(objUsuario => {
-        let fila = cuerpoTabla.insertRow();
-        fila.insertCell().innerHTML = '';
-        fila.insertCell().innerHTML = objUsuario.nombre;
-        fila.insertCell().innerHTML = objUsuario.correo;
-        fila.insertCell().innerHTML = objUsuario.nacimiento;
 
-        fila.insertCell().innerHTML = objUsuario.edad;
-        let celdaTelefonos = fila.insertCell();
-        // Crear un párrafo para cada teléfono
-        objUsuario.telefonos.forEach(objTelefono => {
-            let parrafoTelefono = document.createElement('p');
-            parrafoTelefono.innerHTML = objTelefono.numero;
+const js_limpiar_tabla = () => {
+    tabla.innerHTML = '';
+}
+const js_crear_encabezados_tabla = (data_json) => {
+    // Crear el encabezado de tabla
+    let encabezados = tabla.createTHead();
+    let fila_encabezados = encabezados.insertRow();
+    // Insertar palabras en los encabezados
+    let array_propiedades = Object.keys(data_json[0]);
+    array_propiedades.forEach(propiedad => {
+        propiedad = propiedad.charAt(0).toUpperCase() + propiedad.slice(1)
+        fila_encabezados.insertCell().innerHTML = propiedad;
+    })
+}
+const js_crear_cuerpo_tabla = (data_json) => {
+    // Crear el cuerpo de la tabla 
+    let cuerpoTabla = tabla.createTBody();
 
-            // Agrega el párrafo de teléfono como hijo de la celda
-            celdaTelefonos.appendChild(parrafoTelefono);
-        });
+    // Crear una fila por cada objeto en el data_json
+    data_json.forEach(data_object => {
+        let fila_cuerpo = cuerpoTabla.insertRow();
+        // Crear una celda por cada propiedad del objeto
+        let array_propiedades = Object.keys(data_object);
+        array_propiedades.forEach(propiedad => {
+            fila_cuerpo.insertCell().innerHTML = data_object[propiedad];
+        })
 
-        fila.insertCell().innerHTML = obtenerNombreRol(objUsuario.rol);
-        fila.insertCell().innerHTML = obtenerNombreEstado(objUsuario.estado);
     });
 };
 
+const accion = () => {
+    js_limpiar_tabla();
+    js_crear_encabezados_tabla(listaCartelera);
+    js_crear_cuerpo_tabla(listaCartelera);
+}
 
-document.getElementById("boton-buscar-filtros").onclick = mostrarTabla;
+document.getElementById("boton-buscar-filtros").onclick = accion;
