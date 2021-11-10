2fabcf7a96fd88a1fea90f50a70ddf24324d3e50
adriand00 <amurillog@ucenfotec.ac.cr>
2021-11-09 22:48:32 -0600

    flujo-reservas

--- js/flujo-reservas.js
+++ js/flujo-reservas.js
@@ -1,11 +1,26 @@
 'use strict';
 let stepflag = 1;
+let config_total_colums = 12;
+let config_total_rows = 5;
+let seat_html = '<span class="material-icons chair-button chair-disabled"> <a href="#">chair</a> </span>';
 
+const js_create_seats = (rows, colums) => {
+    let tabla = document.getElementById("tabla-asientos")
+    for (let i = 0; i < rows; i++) {
+        let fila = tabla.insertRow()
+        for (let j = 0; j < colums; j++) {
+            fila.insertCell().innerHTML = seat_html;
+        }
+    }
+
+}
 const js_go_next_step = () => {
     switch (stepflag) {
         case 1:
             document.getElementsByClassName("section-step-one")[0].style.display = "none";
             document.getElementsByClassName("section-step-two")[0].classList.remove("section-step-inactive");
+            document.getElementsByClassName("section-step-two")[0].classList.add("section-step-active");
+            js_create_seats(config_total_rows, config_total_colums);
             stepflag = 2;
             break;
         case 2:
@@ -23,11 +38,5 @@ const js_go_next_step = () => {
     }
 }
 
-const js_apply_display_none = () => {
-    let nodes = document.getElementById('ID_of_parent').childNodes;
-    for (var i = 0; i < nodes.length; i++) {
-        nodes[i].style.display = "none";
-    }
-}
 
 document.getElementById("button-next").onclick = js_go_next_step;
