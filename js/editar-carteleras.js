879a90ca9396d132146d8b84b84676bb1007939f
JRF344 <Joshrf344@gmail.com>
2021-11-09 23:11:26 -0600

    Changes to editar-carteleras

--- js/editar-carteleras.js
+++ js/editar-carteleras.js
@@ -1,24 +1,60 @@
 'use strict';
 
-const botonGuardar = document.querySelector('#btn-cartelera-save');
+const botonGuardar = document.querySelector('#btn-carteleras-guardar');
 
 const selectPelicula = document.querySelector('#select-pelicula');
 const idCartelera = document.querySelector('#txt-id-cartelera');
+const fechaInicio = document.querySelector('#date-cartelera-fecha-inicio');
+const fechaFin = document.querySelector('#date-cartelera-fecha-fin');
+const horaInicio = document.querySelector('#date-cartelera-hora-inicio');
+const horaFin = document.querySelector('#date-cartelera-hora-fin');
 const precioBaseCartelera = document.querySelector('#num-cartelera-precio');
 
 const validar = () => {
     let error = false;
 
-    //Condicion para validar el codigo (SOLO VALIDA SI EXISTE)
-    if (inputCodigo.value == '') {
+    //Condicion para validar el codigo de sala (SOLO VALIDA SI EXISTE)
+    if (idCartelera.value == '') {
         error = true;
         document.querySelector(".input-code").classList.add("input-error");
     } else {
         document.querySelector(".input-code").classList.remove("input-error");
     }
 
+    //Condicion para validar la fecha de inicio (SOLO VALIDA SI NO ES EL DEFAULT)
+    if (fechaInicio.value == '') {
+        error = true;
+        document.querySelector(".input-code").classList.add("input-error");
+    } else {
+        document.querySelector(".input-code").classList.remove("input-error");
+    }
+
+    //Condicion para validar la fecha de fin (SOLO VALIDA SI NO ES EL DEFAULT)
+    if (fechaFin.value == '') {
+        error = true;
+        document.querySelector(".input-code").classList.add("input-error");
+    } else {
+        document.querySelector(".input-code").classList.remove("input-error");
+    }
+
+    //Condicion para validar la hora de inicio (SOLO VALIDA SI NO ES EL DEFAULT)
+    if (horaInicio.value == '') {
+        error = true;
+        document.querySelector(".input-code").classList.add("input-error");
+    } else {
+        document.querySelector(".input-code").classList.remove("input-error");
+    }
+    
+    //Condicion para validar la hora de fin (SOLO VALIDA SI NO ES EL DEFAULT)
+    if (horaFin.value == '') {
+        error = true;
+        document.querySelector(".input-code").classList.add("input-error");
+    } else {
+        document.querySelector(".input-code").classList.remove("input-error");
+    }
 
     if (error == true) {
+        event.preventDefault()
         Swal.fire({
             'icon': 'warning',
             'title': 'La informacion intruducida es invalida',
@@ -27,7 +63,6 @@ const validar = () => {
         });
 
     } else {
-        window.location.href = 'homepage-usuario.html';
     }
 
 };
