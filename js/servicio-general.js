718ff03fd63c26e1cd474885052dc1388c556b40
joaquip <pejoago@gmail.com>
2021-11-15 20:41:14 -0600

    commit to Pedro homagecartelera con nuevo css

--- js/servicio-general.js
+++ js/servicio-general.js
@@ -40,6 +40,59 @@ function comprar() {
     window.location = '../html/flujo-reservas.html';
 }
 
+function historial() {
+    window.location = '../html/historial-reservas.html';
+}
+
+function configUsuario() {
+    window.location = '../html/configuracion-de-usuario.html';
+}
+
+function registroPago() {
+    window.location = '../html/registro-pago.html';
+}
+
+function agregarUsuario() {
+    window.location = '../html/agregar-usuario-admin.html';
+}
+
+function crearCadena() {
+    window.location = '../html/crear-editar-cadena.html';
+}
+
+function reportes() {
+    window.location = '../html/buscar-ver-reportes.html';
+}
+
+function editarCartelera() {
+    window.location = '../html/editar-carteleras.html';
+}
+
+function editarCategoria() {
+    window.location = '../html/editar-categorias.html ';
+}
+
+function editarSala() {
+    window.location = '../html/editar-salas.html';
+}
+
+function editarTipoSala() {
+    window.location = '../html/editar-tipos-salas.html';
+}
+
+function editarTipoAsiento() {
+    window.location = '../html/editar-tipos-de-asiento.html';
+}
+
+function editarTyc() {
+    window.location = '../html/editar-terminos-condiciones.html';
+}
+
+function cartelera() {
+    window.location = '../html/homepagecarteleras.html';
+}
+
+
 function salir() {
     console.log("salir")
 }
