694b166ef979c6b9e1e39977312fec804c77df46
Adriando <58629163+adriand00@users.noreply.github.com>
2021-11-22 18:42:27 -0600

    Footer correction

--- js/navbar-footer.js
+++ js/navbar-footer.js
@@ -9,7 +9,7 @@ const fillNavFooter = () => {
     try { /* Cliente Iniciado */
         divNavbarClienteIniciado.innerHTML = `<header class="header-principal">
                 <div class="logo">
-                    CineManager
+                    Cine Manager
                 </div>
                 <nav>
                     <div class="item">      
@@ -36,7 +36,7 @@ const fillNavFooter = () => {
     try { /* Cliente NO Iniciado*/
         divNavbarClienteNoIniciado.innerHTML = `<header class="header-principal">
                     <div class="logo">
-                    CineManager
+                    Cine Manager
                     </div>
 
                     <nav>
@@ -59,7 +59,7 @@ const fillNavFooter = () => {
     try { /* Admin Iniciado */
         divNavbarAdminIniciado.innerHTML = `<header class="header-principal">
                 <div class="logo">
-                    CineManager
+                    Cine Manager
                 </div>
                 <nav>
                     <div class="item">      
@@ -94,17 +94,11 @@ const fillNavFooter = () => {
         <footer class="footer">
                <div class="contain">
                   <div class="col">
-                     <p>Cine REX</p>
+                     <p>Cine Manager</p>
                      <ul>
                         <li><a href="../html/tyc.html">Terminos y condiciones</a></li>
                      </ul>
                   </div>
-                  <!-- <div class="col">
-                     <p>Cine REX</p>
-                     <ul>
-                        <li><a href="">Terminos y condiciones</a></li>
-                     </ul>
-                  </div> -->
                </div>
         </footer>`;
     } catch (err) {
@@ -113,163 +107,3 @@ const fillNavFooter = () => {
 }
 
 fillNavFooter();
-
-/*
-
-const divNavbarClienteIniciado = document.querySelector(".navbar-cliente-iniciado");
-const divNavbarClienteNoIniciado = document.querySelector(".navbar-cliente-no-iniciado");
-const divNavbarAdminIniciado = document.querySelector(".navbar-admin-iniciado");
-const divNavbarAdminNoIniciado = document.querySelector(".navbar-admin-no-iniciado");
-const footerCliente = document.querySelector(".footer-cliente");
-const footerAdmin = document.querySelector(".footer-admin");
-
-const fillNavFooter = () => {
-    try {
-        divNavbarClienteIniciado.innerHTML = `<header class="header-principal">
-                <div class="logo">
-                    Cine Manager
-                </div>
-                <nav>
-                    <div class="item">      
-                        <span class="material-icons">
-                        <a href="landing-page.html">chevron_left</a>
-                        </span>
-                    </div>   
-                    <div class="push">                
-                        <div class="item">
-                            <span class="material-icons md-light">account_circle</span>
-                            <a class="item" href="../html/landing-page.html">Cerrar sesión</a>
-                        </div>  
-                    </div>
-                </nav>    
-                </header>`;
-    } catch (err) {
-
-    }
-
-    try {
-        divNavbarClienteNoIniciado.innerHTML = `<header class="header-principal">
-                    <div class="logo">
-                    Cine Manager
-                    </div>
-
-                    <nav>
-                        <div class="item">      
-                        <span class="material-icons md-light">home</span>          
-                        <a href="../html/homepage-usuario.html">Inicio</a>
-                        </div>     
-                        <div class="item">
-                        <span class="material-icons md-light">camera_indoor</span>
-                        <a class="item" href="../html/buscar-listar.html">Busqueda</a>
-                        </div>    
-                        <div class="item">
-                        <span class="material-icons md-light">event_seat</span>
-                        <a class="item" href="../html/homepagecarteleras.html">Reservas</a>
-                        </div>
-    
-                        <div class="push">
-                        <div class="item">
-                            <span class="material-icons md-light">info</span>
-                            <a class="item" href="../html/tyc.html">Acerca de</a>
-                        </div>
-                        <div class="item">
-                            <span class="material-icons md-light">account_circle</span>
-                            <a class="item" href="../html/iniciar-sesion.html">Iniciar sesion</a>
-                        </div>  
-                        </div>
-                    </nav>    
-                </header>`;
-    } catch (err) {
-
-    }
-
-    try {
-        divNavbarAdminIniciado.innerHTML = `<header class="header-principal">
-                <div class="logo">
-                    Cine Manager
-                </div>
-                <nav>
-                <div class="push">                 
-                    <div class="item">
-                    <span class="material-icons md-light">account_circle</span>
-                    <a class="item" href="../html/landing-page.html">Cerrar sesion</a>
-                    </div>  
-                </div>
-                </nav>    
-            </header>`;
-    } catch (err) {
-
-    }
-
-    try {
-        divNavbarAdminNoIniciado.innerHTML = `<header class="header-principal">
-                <div class="logo">
-                    Cine Manager
-                </div>
-                <nav>
-                <div class="push">
-                    <div class="item">
-                    <span class="material-icons md-light">info</span>
-                    <a class="item" href="../html/tyc.html">Acerca de</a>
-                    </div>
-                    <div class="item">
-                    <span class="material-icons md-light">account_circle</span>
-                    <a class="item" href="../html/iniciar-sesion.html">Iniciar sesion</a>
-                    </div>  
-                </div>
-                </nav>    
-                </header>`;
-    } catch (err) {
-
-    }
-
-    try {
-        footerCliente.innerHTML = `
-        <footer class="footer">
-               <div class="contain">
-                  <div class="col">
-                     <p>Cine REX</p>
-                     <ul>
-                        <li><a href="../html/tyc.html">Terminos y condiciones</a></li>
-                     </ul>
-                  </div>
-                  <!-- <div class="col">
-                     <p>Cine REX</p>
-                     <ul>
-                        <li><a href="">Terminos y condiciones</a></li>
-                     </ul>
-                  </div> -->
-               </div>
-        </footer>`;
-    } catch (err) {
-
-    }
-
-    try {
-        footerAdmin.innerHTML = `
-        <footer class="footer">
-               <div class="contain">
-                  <div class="col">
-                     <p>Cine REX</p>
-                     <ul>
-                        <li><a href="../html/tyc.html">Terminos y condiciones</a></li>
-                     </ul>
-                  </div>
-                  <!-- <div class="col">
-                     <p>Cine REX</p>
-                     <ul>
-                        <li><a href="">Terminos y condiciones</a></li>
-                     </ul>
-                  </div> -->
-               </div>
-        </footer>`;
-    } catch (err) {
-
-    }
-
-
-}
-
-fillNavFooter();
-
-*/
