512fdb4e0a1526ccbee65bbb6eafaf4e99f77ff2
joaquip <pejoago@gmail.com>
2021-12-14 23:01:02 -0600

    commit to pedro db

--- js/servicio-tyc.js
+++ js/servicio-tyc.js
@@ -1,7 +1,28 @@
 'use strict'
 
+
+const leerDatos = async(pEndPoint) => {
+    let url = `http://localhost:3000/api${pEndPoint}`;
+    let msj = '';
+
+    await axios({
+        method: 'get',
+        url: url,
+
+    }).then(response => {
+        msj = response.data.lista[0].terminos;
+    });
+
+    return msj;
+};
+
 const terminosCondiciones = document.getElementById("tyc-body")
 
-let terminos = await leerDatos('/obtener-terminos');
+let terminos = '';
 
-terminosCondiciones.innerHTML = terminos;
+const cargarDatos = async() => {
+    terminos = await leerDatos('/obtener-terminos');
+    terminosCondiciones.innerHTML = terminos;
+}
+
+cargarDatos();
