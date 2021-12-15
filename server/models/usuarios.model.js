84f1745405d98992fda9b85a5f7efc8d5fd5a27e
Adriando <58629163+adriand00@users.noreply.github.com>
2021-12-14 22:03:56 -0600

    agregando metodos de pago a usuario

--- server/models/usuarios.model.js
+++ server/models/usuarios.model.js
@@ -12,6 +12,17 @@ let schemaUsuarios = new mongoose.Schema({
     numeroId: { type: String, required: true, unique: true },
     correoUsuario: { type: String, required: true, unique: true },
     passwordUsuario: { type: String, required: true },
+    metodos_pago: [{
+        numero: { type: String, required: true },
+        mes: { type: Number, required: true },
+        anno: { type: Number, required: true },
+        cvc: { type: Number, required: true },
+        nombre_titular: { type: String, required: true },
+        direccion: { type: String, required: true },
+        provincia: { type: String, required: true },
+        canton: { type: String, required: true },
+        distrito: { type: String, required: true }
+    }]
 });
 
 module.exports = new mongoose.model('Usuario', schemaUsuarios, 'usuarios');
