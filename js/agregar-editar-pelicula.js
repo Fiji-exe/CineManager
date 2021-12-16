448ab3d459d2011edac9c848bb574a75a7306f27
Adriando <58629163+adriand00@users.noreply.github.com>
2021-12-15 23:05:59 -0600

    tamos

--- js/agregar-editar-pelicula.js
+++ js/agregar-editar-pelicula.js
@@ -1,6 +1,33 @@
 'use strict'
 
 const buttonSubirImagenView = document.getElementById("btn_cargar_imagen");
+const imgCadena = document.getElementById("imagen-peli");
+const buttonGuardar = document.getElementById("btn-crear-peli");
+
+/*
+ *
+ * Obtener Categorias
+ *
+ *
+ */
+
+const recuperar_categorias_de_BD = async() => {
+    return await obtenerCategorias();
+}
+
+const insertar_categorias = (lista) => {
+        let opciones = "<option disabled selected>Elige una categoría</option>";
+        let opciones_value = 0;
+        lista.forEach(element => {
+            opciones_value++;
+            opciones += `<option value="${element._id}"> ${element.categoria} </option>`
+        });
+
+        document.getElementById('select-categorias').innerHTML = opciones;
+    }
+    //*prueba
+recuperar_categorias_de_BD().then((res) => { insertar_categorias(res) });
+
 
 /**
  * 
@@ -22,31 +49,40 @@ buttonSubirImagenView.addEventListener('click', () => {
     widget_cloudinary.open();
 }, false);
 
-
-
-
 /*
  *
- * Obtener Categorias
- *
+ * Vacios
+ * Numeros negativos
  *
  */
 
-const recuperar_categorias_de_BD = async() => {
-    return await obtenerCategorias();
+const validar_inputs = () => {
+    let validacion = true;
+    /* LIMPIAR PREVIA VALIDACION */
+    let nodelist_inputs_requeridos = document.querySelectorAll('[required]');
+    nodelist_inputs_requeridos.forEach(input => {
+        input.classList.remove('userinput-error');
+        input.parentNode.classList.remove('userinput-error');
+    });
+
+    /* Checar vacios */
+    nodelist_inputs_requeridos.forEach(input => {
+        if (input.value === '') {
+            input.classList.add('userinput-error');
+            input.parentNode.classList.add('userinput-error');
+        }
+    });
+
+
+
+
+    Swal.fire({
+        'icon': 'success',
+        'title': 'Éxito',
+        'text': 'Película agregada correctamente',
+        'confirmButtonText': 'Entendido'
+    });
+
 }
 
-const insertar_categorias = (lista) => {
-        let opciones = "<option disabled selected>Elige una categoría</option>";
-        let opciones_value = 0;
-        lista.forEach(element => {
-            opciones_value++;
-            opciones += `<option value="${opciones_value}"> ${element.categoria} </option>`
-        });
-
-        console.log(opciones)
-
-        document.getElementById('select-categorias').innerHTML = opciones;
-    }
-    //*prueba
-recuperar_categorias_de_BD().then((res) => { insertar_categorias(res) });
+buttonGuardar.addEventListener('click', validar_inputs);
