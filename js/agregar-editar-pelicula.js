1c23878526f588700c6bdc272f130fff36492cca
Adriando <58629163+adriand00@users.noreply.github.com>
2021-12-15 20:23:38 -0600

    quicksave

--- js/agregar-editar-pelicula.js
+++ js/agregar-editar-pelicula.js
@@ -1,3 +1,17 @@
 'use strict'
 
-//* Validaciones
+/*
+ *
+ * Obtener Categorias
+ *
+ *
+ */
+
+let categorias = [];
+
+for (let i = 0; i < categorias.length; i++) {
+    let nuevaCategoria = `<option value="${i}"> ${categorias[i]} </option> `;
+
+}
+
+document.getElementById('select-categorias').innerHTML =
