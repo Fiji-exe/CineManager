03ecb85c0fac9000d03fc43d3c2b5f5f937937b5
joaquip <pejoago@gmail.com>
2021-12-16 22:39:37 -0600

    commit de pedro a dev (terminos y condiciones)

--- server/routes/terminos-condiciones.route.js
+++ server/routes/terminos-condiciones.route.js
@@ -10,33 +10,33 @@ router.post('/registrar-terminos', (req, res) => {
         terminos: req.body.terminos
     });
 
-    nuevosTerminos.save( (error) => {
-        if(error){
+    nuevosTerminos.save((error) => {
+        if (error) {
 
             res.json({
                 msj: 'Ocurrio un error al registrar',
                 error
             });
 
-        }
-        else{
+        } else {
             res.json({
-                msj: 'Se registro exitosamente'               
+                msj: 'Se registro exitosamente'
             });
         }
-    } );
+    });
 
 });
 
 router.get('/obtener-terminos', (req, res) => {
-    terminosyCondiciones.find((error, lista) =>{
-        if(error){
+    terminosyCondiciones.find((error, lista) => {
+        if (error) {
 
             res.json({
                 msj: 'Fallo la consulta',
                 error
             });
-        else{
+
+        } else {
 
             res.json({
                 msj: 'terminos listados correctamente',
@@ -51,17 +51,16 @@ router.put('/modificar-terminos', (req, res) => {
         terminos: req.body.terminos
     }
 
-    terminosyCondiciones.updateOne({_id: req.body._id}, datos, error => {
-        if(error){
+    terminosyCondiciones.updateOne({ _id: req.body._id }, datos, error => {
+        if (error) {
 
             res.json({
                 msj: 'Ocurrio un error al actualizar',
                 error
             });
-        }
-        else{
+        } else {
             res.json({
-                msj: 'Se actualizo exitosamente'               
+                msj: 'Se actualizo exitosamente'
 
             });
         }
